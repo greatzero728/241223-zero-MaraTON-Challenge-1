fc713fbf558695e7e3c4906247a417c5a2c3f70c
greatzero728 <happyzero728@gmail.com>
2024-12-24 22:15:24 +0900

    no algorithm implementation.

---
+++ Solution/The_Best_Lusk/241224 1852 100000.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<memory>
+#include<utility>
+#include<algorithm>
+
+using namespace std;
+
+const int NN = 2121212;
+
+char s[NN];
+
+int main() {
+  scanf("%s", s);
+  scanf("%s", s);
+  printf("%s", s);
+}
