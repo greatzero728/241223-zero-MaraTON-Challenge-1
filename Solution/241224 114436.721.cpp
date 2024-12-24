4d97e21fae21741548aaa38662e65376327ca324
greatzero728 <happyzero728@gmail.com>
2024-12-24 18:40:52 +0900

    The given template source code using lz4 algorithm

---
+++ Solution/241224 114436.721.cpp
@@ -0,0 +1,46 @@
+/*
+ * solution.cpp
+ *
+ * Example solution.
+ * This is (almost) how blocks are actually compressed in TON.
+ * Normally, blocks are stored using vm::std_boc_serialize with mode=31.
+ * Compression algorithm takes a block, converts it to mode=2 (which has less extra information) and compresses it using lz4.
+ */
+#include <bits/stdc++.h>
+#include "td/utils/lz4.h"
+#include "td/utils/base64.h"
+#include "vm/boc.h"
+
+using namespace std;
+
+td::BufferSlice compress(td::Slice data) {
+  td::Ref<vm::Cell> root = vm::std_boc_deserialize(data).move_as_ok();
+  td::BufferSlice serialized = vm::std_boc_serialize(root, 2).move_as_ok();
+  return td::lz4_compress(serialized);
+}
+
+td::BufferSlice decompress(td::Slice data) {
+  td::BufferSlice serialized = td::lz4_decompress(data, 2 << 20).move_as_ok();
+  auto root = vm::std_boc_deserialize(serialized).move_as_ok();
+  return vm::std_boc_serialize(root, 31).move_as_ok();
+}
+
+int main() {
+  string mode;
+  cin >> mode;
+  CHECK(mode == "compress" || mode == "decompress");
+
+  string base64_data;
+  cin >> base64_data;
+  CHECK(!base64_data.empty());
+
+  td::BufferSlice data(td::base64_decode(base64_data).move_as_ok());
+
+  if (mode == "compress") {
+    data = compress(data);
+  } else {
+    data = decompress(data);
+  }
+
+  cout << td::base64_encode(data) << endl;
+}
